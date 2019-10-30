909d2e7160c38470a5d1838e9a8f02803c4107e6
Brown <oso7coda@gmail.com>
2019-10-30 15:55:55 +0900

    add premanent, secure, httponly

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -3,11 +3,20 @@ var cookie = require('cookie')
  http.createServer(function(request, respons){
      console.log(request.headers.cookie);
      var cookies = {};
+
      if(request.headers.cookie!==undefined){
          var cookies = cookie.parse(request.headers.cookie);
          console.log(cookies.yummy_cookie)
-     }
-     respons.writeHead(200,{'Set-Cookie':['yummy_cookie=choco','tasty_cookie=strawberry']})
+        }
+        respons.writeHead(200,{'Set-Cookie':
+            [
+                'yummy_cookie=choco'
+                ,'tasty_cookie=strawberry'
+                ,`Permanent=cookies;Max-Age=${60*60*23*30}`
+                ,"Secure=secure; Secure"
+                ,"HttpOnly=HttpOnly; HttpOnly"
+            ]}
+        ) 
      respons.end('Cookie!!')
      
  }).listen(3000);
